d5f968a2698d2d899b02251b68d2511e39e82b45
dhakalsamundra <seapush02@gmail.com>
2020-10-07 18:47:30 +0300

    add new data

--- FullStack-Part4-master/tests/test_helper.js
+++ FullStack-Part4-master/tests/test_helper.js
@@ -3,69 +3,33 @@ const User = require('../models/user')
 
 const initialBlogs = [
   {
-    _id: '5e98061c84cc120a0d6d7fad',
-    title: 'Murphy Law Defination',
+    _id: '5f7d91702f69e1a09f09925f',
+    title: 'data info',
     author: 'Samundra',
-    url: 'https://en.wikipedia.org/wiki/Murphy%27s_law',
+    url: 'www.xxx.com',
     likes: 20,
-    __v: 0
   },
   {
-    _id: '5e980aa937dbb30b51ce6e27',
-    title: 'Featuring the HomeSense',
-    author:'Sandhya',
-    url:'https://www.capturebylucy.com/blog',
-    likes:120,
-    __V: 0
+    _id: '5f7d91832f69e1a09f099260',
+    title: 'data science',
+    author:'Sandesh',
+    url:'www.xsandeshxx.com',
+    likes:2
   },
   {
     _id: '5e980af337dbb30b51ce6e28',
-    title: 'Interior Designing',
-    author: 'Sandesh',
-    url: 'https://www.thegoodtrade.com/',
-    likes: 120,
-    __v: 0
+    title: 'bio science',
+    author: 'sandhya',
+    url: 'www.sandhya.com',
+    likes: 12
   },
   {
-    _id: '5e9825155bb9db121455fb0c',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
+    _id: '5f7d91a92f69e1a09f099262',
+    title: 'art and design',
+    author: 'yojana',
+    url: 'www.yojana.com',
+    likes: 120
   },
-  {
-    _id: '5e9825335bb9db121455fb0d',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5e9825475bb9db121455fb0e',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5e98255a5bb9db121455fb0f',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5e9825685bb9db121455fb10',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  }
 ]
 
 const nonExixtingId = async () => {
