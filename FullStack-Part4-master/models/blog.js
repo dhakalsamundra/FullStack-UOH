52ba739c0be6b7272e01e34427d410f42837ede6
dhakalsamundra <seapush02@gmail.com>
2020-10-08 13:09:16 +0300

    reference connectin

--- FullStack-Part4-master/models/blog.js
+++ FullStack-Part4-master/models/blog.js
@@ -24,10 +24,10 @@ const blogSchema = new mongoose.Schema({
     type: Number,
     default: 0
   },
-  // user: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'User'
-  // }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 
