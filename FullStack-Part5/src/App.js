b204d9dd33a8c1a7cb9e40c37b1041853f60690c
dhakalsamundra <seapush02@gmail.com>
2020-10-13 13:39:24 +0300

    add component

--- FullStack-Part5/src/App.js
+++ FullStack-Part5/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react'
+
+import AddBlog from './components/AddBlog'
+import Login from './components/Login'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
@@ -17,6 +20,8 @@ const App = () => {
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+      <AddBlog />
+      <Login />
     </div>
   )
 }
