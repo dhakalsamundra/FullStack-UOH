1ba7042e3c0265f3c201133301612003db4b5039
dhakalsamundra <seapush02@gmail.com>
2020-10-14 12:00:53 +0300

    add toggable to login

--- FullStack-Part5/src/App.js
+++ FullStack-Part5/src/App.js
@@ -3,9 +3,14 @@ import React, { useState, useEffect } from 'react'
 import Login from './components/Login'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import AddBlog from './components/AddBlog'
+import Notification from './components/Notification'
+import Togglable from './components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,13 +18,32 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(()=>{
+    const loggedUser = window.localStorage.getItem('token')
+    if(loggedUser){
+      const user = JSON.parse(loggedUser)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedUser');
+    setSuccessMessage(`${user.name} logOut successful`)
+    setTimeout(() => {
+      setSuccessMessage(null);
+    }, 5000);
+    setUser(null);
+  };
+
   return (
     <div>
+      <Notification successMessage={successMessage} />
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
-      <Login />      
+      {user === null ? <Togglable buttonLabel = 'Login'><Login /> </Togglable>: <div><p>{user.name} logged in <button onClick={handleLogout}>logout</button></p><AddBlog /></div>}     
     </div>
   )
 }
