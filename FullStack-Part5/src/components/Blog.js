b01db0b0d8288c431edf4269847e39a3745ac355
dhakalsamundra <seapush02@gmail.com>
2020-10-14 19:35:00 +0300

    add inline styling

--- FullStack-Part5/src/components/Blog.js
+++ FullStack-Part5/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, {useState} from 'react'
+import Togglable from './Togglable'
+import EachBlog from './EachBlog'
+
+const Blog = ({ blog, user }) => {
+
+  const [toggleButton, setToggleButton] = useState(false)
+
+  const button = () => {
+    setToggleButton(!toggleButton)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+  <div style={blogStyle}>
+    {blog.title} written by {blog.author}
+    {toggleButton ? (
+      <div>
+        <button onClick={button}>Hide</button><EachBlog blog={blog} /></div>) : (
+        <button onClick={button}>Show</button>)}
   </div>
 )
+}
 
 export default Blog
