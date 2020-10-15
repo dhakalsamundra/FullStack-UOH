cb98f606963f28864f21726c88d7616ab5a99011
dhakalsamundra <seapush02@gmail.com>
2020-10-15 11:25:27 +0300

    done upto exercise 5.10

--- FullStack-Part5/src/components/EachBlog.js
+++ FullStack-Part5/src/components/EachBlog.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-export default function EachBlog({ blog }) {
+import blogService from '../services/blogs'
+
+
+export default function EachBlog({ blog, user }) {
+
     const blogStyle = {
         paddingTop: 10,
         paddingLeft: 2,
@@ -8,14 +12,36 @@ export default function EachBlog({ blog }) {
         borderWidth: 1,
         marginBottom: 5
       }
+      const addlikes = () => {
+          const object = {
+              id: blog.id,
+              user: blog.user.id,
+              author: blog.author,
+              url: blog.url,
+              title: blog.title,
+              likes: parseInt(blog.likes) + 1
+
+          }
+          blogService.update(object)
+      }
+
+      const handleDelete = () => {
+        const id = blog.id
+        if (window.confirm(`Do you want to delete ${blog.title}?`)) {
+        blogService.deleteBlog(id)
+      }
+    }
   return (
     <div style={blogStyle}>
       <ul>
-        <li>Likes: {blog.likes}</li>
+        <li>Likes: {blog.likes}<button onClick={addlikes}>Like</button></li>
         <li>Url: {blog.url}</li>
         <li>Title: {blog.title}</li>
         <li>Author: {blog.author}</li>
       </ul>
+      {user.username === blog.user.username ? 
+        <button onClick={handleDelete}>Delete</button>   :  ''
+    }
     </div>
   );
 }
