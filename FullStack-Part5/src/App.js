3595539f355b9ff1e1366a05d887c30bdb6b88b4
dhakalsamundra <seapush02@gmail.com>
2020-10-16 14:58:36 +0300

    add function to the app component

--- FullStack-Part5/src/App.js
+++ FullStack-Part5/src/App.js
@@ -1,51 +1,151 @@
-import React, { useState, useEffect } from 'react'
-
-import Login from './components/Login'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
-import AddBlog from './components/AddBlog'
-import Notification from './components/Notification'
-import Togglable from './components/Togglable'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import Togglable from './components/Togglable';
+import Login from './components/Login';
+import AddBlog from './components/AddBlog';
+import Notification from './components/Notification';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [user, setUser] = useState(null)
-  const [successMessage, setSuccessMessage] = useState(null)
+  const [notification, setNotification] = useState(null);
+  const [user, setUser] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+
+  const blogFormRef = React.createRef();
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    const getBlog = async () => {
+      const blogs = await blogService.getAll();
+      setBlogs(blogs);
+    };
+    getBlog();
+  }, []);
 
-  useEffect(()=>{
-    const loggedUser = window.localStorage.getItem('token')
-    if(loggedUser){
-      const user = JSON.parse(loggedUser)
-      setUser(user)
-      blogService.setToken(user.token)
+  useEffect(() => {
+    const loggedUserJson = window.localStorage.getItem('loggedUser');
+    if (loggedUserJson) {
+      const user = JSON.parse(loggedUserJson);
+      setUser(user);
+      blogService.setToken(user.token);
     }
-  }, [])
+  }, []);
+
+  const handleNotification = (message, type = 'error') => {
+    setNotification({ message, type });
+    setTimeout(() => {
+      setNotification(null);
+    }, 5000);
+  };
+
+  const loginUser = async (credentials) => {
+    try {
+      const user = await loginService.login(credentials);
+      window.localStorage.setItem('loggedUser', JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+    } catch (error) {
+      const message = error.response.data.error;
+      handleNotification(message);
+      setTimeout(() => {
+        handleNotification(null);
+      }, 5000);
+    }
+  };
+
+  const addNewBlog = async (newBlog) => {
+    blogFormRef.current.toggleVisibility();
+    try {
+      const returnedBlog = await blogService.create(newBlog);
+      const message = `${returnedBlog.title} by ${returnedBlog.author} successfully added`;
+      handleNotification(message, 'success');
+      setBlogs([...blogs, returnedBlog]);
+    } catch (error) {
+      // const message = error.response.data.error;
+      const message = error
+      handleNotification(message);
+      setTimeout(() => {
+        handleNotification(null);
+      }, 5000);
+    }
+  };
+
+  const handleDelete = async (toRemoveBlog) => {
+    console.log('delete this', toRemoveBlog)
+    if (user === null) {
+      handleNotification('Not Authorized');
+      return setTimeout(() => {
+        handleNotification(null);
+      }, 5000);
+    }
+
+    try {
+      const remMsg = `Remove ${toRemoveBlog.title} by ${toRemoveBlog.author}?`;
+      const userChoice = window.confirm(remMsg);
+      if (!userChoice) {
+        return null;
+      }
+      await blogService.remove(toRemoveBlog);
+      const newBlogs = blogs.filter((blog) => toRemoveBlog.id !== blog.id);
+      setBlogs(newBlogs);
+      handleNotification('blog removed sucessfully', 'success');
+    } catch (error) {
+      const message = error.response.data.error;
+      handleNotification(message);
+      setTimeout(() => {
+        handleNotification(null);
+      }, 5000);
+    }
+  };
 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedUser');
-    setSuccessMessage(`${user.name} logOut successful`)
-    setTimeout(() => {
-      setSuccessMessage(null);
-    }, 5000);
     setUser(null);
   };
 
+  const loginForm = () => {
+    return (
+      <Togglable buttonLabel='Login'>
+        <Login loginUser={loginUser} />
+      </Togglable>
+    );
+  };
+
+  const blogForm = () => {
+    return (
+      <Togglable buttonLabel='Add Blog' ref={blogFormRef}>
+        <AddBlog addNewBlog={addNewBlog} />
+      </Togglable>
+    );
+  };
+
+  const addLikes = async (toUpdateBlog) => {
+    const returnedBlog = await blogService.update(toUpdateBlog);
+    const newBlogs = blogs.filter((blog) => blog.id !== toUpdateBlog.id);
+    const sortedBlogs = ([...newBlogs, returnedBlog]);
+    setBlogs(sortedBlogs);
+  };
   return (
     <div>
-      <Notification successMessage={successMessage} />
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} user={user} />
-      )}
-      {user === null ? <Togglable buttonLabel = 'Login'><Login /> </Togglable>: <div><p>{user.name} logged in <button onClick={handleLogout}>logout</button></p><Togglable buttonLabel='Add Blog'><AddBlog /></Togglable></div>}     
-    </div>
-  )
-}
+      <Notification notification={notification} />
 
-export default App
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          {user.name} logged in <button onClick={handleLogout}>logout</button>
+          {blogForm()}
+        </div>
+      )}
+
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          <Blog {...{ blog, addLikes, handleDelete, user }} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
