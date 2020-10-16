2cd0f56241edd3aaeb320e4dcc605fb58ff3defc
dhakalsamundra <seapush02@gmail.com>
2020-10-16 15:02:22 +0300

    refactor to get the id for post request

--- FullStack-Part5/src/services/blogs.js
+++ FullStack-Part5/src/services/blogs.js
@@ -1,35 +1,40 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+const baseUrl = '/api/blogs';
 
-let token = null
-const setToken = newToken => {
-  token = `bearer ${newToken}`
-}
+let token = null;
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const getAll = async () => {
+  const request = await axios.get(baseUrl);
+  return request.data;
+};
 
-const create = async Object => {
-    const config = { headers: {Authorization: token}}
-    const response = axios.post(baseUrl, Object, config)
-    return response.data
-}
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+};
 
-const update = ( Object)=> {
-  const id = Object.id
-    const response = axios.put(`${baseUrl}/${id}`, Object)
-    return response.data
-}
-
-const deleteBlog = async (id) => {
-  console.log('this is the delete blog id', id)
+const create = async (newObject) => {
   const config = {
-    headers: {Authorization: token}
-  }
-  const response = axios.delete(`${baseUrl}/${id}`, config)
-  return response.data
-}
+    headers: { Authorization: token },
+  };
+  const response = await axios.post(baseUrl, newObject, config);
+  return response.data;
+};
 
-export default { getAll, create, update, setToken, deleteBlog }
+const remove = async (removeObject) => {
+  console.log('this is service delte', removeObject)
+  const id = removeObject.id
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.delete(baseUrl.concat(`/${id}`), config);
+  return response.data;
+};
+
+const update = async (newObject) => {
+  newObject = { ...newObject, user: newObject.user.id };
+  const id = newObject.id;
+  const response = await axios.put(baseUrl.concat(`/${id}`), newObject);
+  return response.data;
+};
+
+export default { getAll, setToken, create, update, remove };
