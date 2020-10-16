b1794e8c6e5cfd3cc572956dc477b88f22da008d
dhakalsamundra <seapush02@gmail.com>
2020-10-16 15:00:09 +0300

    refactor the functionality

--- FullStack-Part5/src/components/EachBlog.js
+++ FullStack-Part5/src/components/EachBlog.js
@@ -1,10 +1,23 @@
 import React from "react";
 
-import blogService from '../services/blogs'
+export default function EachBlog({ blog, addLikes, handleDelete, user}) {
 
+  const onChangeDelete = e => {
+    const data = blog
+    handleDelete(data)
+  }
+  const onChangeLikes = e => {
+    const object = {
+      id: blog.id,
+      user: blog.user.id,
+      author: blog.author,
+      url: blog.url,
+      title: blog.title,
+      likes: parseInt(blog.likes) + 1
 
-export default function EachBlog({ blog, user }) {
-
+  }
+  addLikes(object)
+  }
     const blogStyle = {
         paddingTop: 10,
         paddingLeft: 2,
@@ -12,35 +25,16 @@ export default function EachBlog({ blog, user }) {
         borderWidth: 1,
         marginBottom: 5
       }
-      const addlikes = () => {
-          const object = {
-              id: blog.id,
-              user: blog.user.id,
-              author: blog.author,
-              url: blog.url,
-              title: blog.title,
-              likes: parseInt(blog.likes) + 1
-
-          }
-          blogService.update(object)
-      }
-
-      const handleDelete = () => {
-        const id = blog.id
-        if (window.confirm(`Do you want to delete ${blog.title}?`)) {
-        blogService.deleteBlog(id)
-      }
-    }
   return (
     <div style={blogStyle}>
       <ul>
-        <li>Likes: {blog.likes}<button onClick={addlikes}>Like</button></li>
+        <li>Likes: {blog.likes}<button onClick={onChangeLikes}>Like</button></li>
         <li>Url: {blog.url}</li>
         <li>Title: {blog.title}</li>
         <li>Author: {blog.author}</li>
       </ul>
       {user.username === blog.user.username ? 
-        <button onClick={handleDelete}>Delete</button>   :  ''
+        <button onClick={onChangeDelete}>Delete</button>   :  ''
     }
     </div>
   );
