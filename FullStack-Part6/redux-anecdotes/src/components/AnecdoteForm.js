a8ca7dddb990ce418e3217374e39314395ccbf8b
dhakalsamundra <seapush02@gmail.com>
2020-10-22 16:54:30 +0300

    add component

---
+++ FullStack-Part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function AnecdoteForm() {
+    return (
+        <div>
+            <h2>Create New Note</h2>
+        <form>
+          <div>
+            <input />
+          </div>
+          <button>create</button>
+        </form>
+        </div>
+    )
+}
