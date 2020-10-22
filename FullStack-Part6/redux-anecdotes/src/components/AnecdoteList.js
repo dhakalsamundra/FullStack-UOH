5c30ad218a6f3d208ac455026c0cedf830aae940
dhakalsamundra <seapush02@gmail.com>
2020-10-22 22:27:57 +0300

    data list component

--- FullStack-Part6/redux-anecdotes/src/components/AnecdoteList.js
+++ FullStack-Part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,26 +1,25 @@
-import React from "react";
-import { useSelector, useDispatch } from 'react-redux'
+import React from 'react';
+import Button from './Button';
+import { useSelector } from 'react-redux';
 
+const AnecdoteList = () => {
+  const data = useSelector((state) =>
+    state.data.sort((a, b) => b.votes - a.votes)
+  );
 
-export default function AnecdoteList() {
-  const anecdotes = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    console.log("vote", id);
-  };
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+      {data.map((element) => {
+        return (
+          <div key={element.id}>
+            {element.content}<br></br>
+            {element.votes} votes 
+            <Button element={element} />
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
-}
+};
+
+export default AnecdoteList;
