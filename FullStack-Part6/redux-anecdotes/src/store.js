a887424678cd9c53b392fac8a764d9619d2c8d5a
dhakalsamundra <seapush02@gmail.com>
2020-10-26 18:05:50 +0200

    setup store

--- FullStack-Part6/redux-anecdotes/src/store.js
+++ FullStack-Part6/redux-anecdotes/src/store.js
@@ -2,8 +2,14 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
+import anecdotesReducer from './reducers/anecdoteReducer';
+import notificationReducer from './reducers/notificationReducer';
+import filterReducer from './reducers/filterReducer';
 
 const reducer = combineReducers({
+  data: anecdotesReducer,
+  notification: notificationReducer,
+  filteredData: filterReducer,
 });
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
