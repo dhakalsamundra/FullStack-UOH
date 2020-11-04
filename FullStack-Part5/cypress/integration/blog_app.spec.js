55e61d84cdd441afdfbb37b1cb474705cb5d013f
dhakalsamundra <seapush02@gmail.com>
2020-11-04 21:35:58 +0200

    beforeEach route

--- FullStack-Part5/cypress/integration/blog_app.spec.js
+++ FullStack-Part5/cypress/integration/blog_app.spec.js
@@ -1,12 +1,51 @@
 describe('Blog app', function(){
     beforeEach(function() {
+        cy.request('POST', 'http://localhost:3002/api/test/reset')
+        const user = {
+            name: 'Cypress',
+            username: 'Cypress',
+            password: 'Cypress'
+        }
+        cy.request('POST', 'http://localhost:3002/api/users/', user)
         cy.visit('http://localhost:3000')
       })
 
     it('front page can be opened', () => {
         cy.contains('blogs')
     })
-    it('login form can be opened', function(){
-        cy.contains('Login').click()
-    }) 
-})
+    it('valid user can be logged in', function () {
+        cy.get('#togglable-btn').click()
+        cy.get('#username').type('Cypress')
+        cy.get('#password').type('Cypress')
+        cy.get('#login-button').click()
+    
+        cy.contains('Samundra logged in')
+      });
+      it('login user can be logged in', function () {
+        cy.get('#togglable-btn').click()
+        cy.get('#username').type('abc')
+        cy.get('#password').type('def')
+        cy.get('#login-button').click()
+    
+        cy.contains('wrong credentials')
+      });
+    
+    describe('when logged in', function(){
+        beforeEach(function(){
+            cy.contains('Login').click()
+            cy.get('#username').type('Samundra')
+            cy.get('#password').type('Samundra')
+            cy.get('#login-button').click()
+          })
+      
+          it('a new blog can be created', function() {
+            cy.contains('Add Blog').click()
+            cy.get('#Title').type('Cypress');
+            cy.get('#Author').type('Samundra');
+            cy.get('#Url').type('cypress.com');
+            cy.get('#Likes').type(20);
+            cy.get('#addblog-btn').click()
+            cy.contains('Cypress')
+          })
+        })
+    })
