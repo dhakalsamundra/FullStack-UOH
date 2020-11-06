318565555c5f0e0fbc4eb82403ba7376d2cdfdd1
dhakalsamundra <seapush02@gmail.com>
2020-11-06 08:38:08 +0200

    refactor

--- FullStack-Part5/src/components/Togglable.js
+++ FullStack-Part5/src/components/Togglable.js
@@ -1,26 +1,26 @@
-import React, { useState, useImperativeHandle } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
 
 const Togglable = React.forwardRef((props, ref) => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false)
 
   /**
-   * ! "visible" in displayWhenVisible refers to the item which is toggled
+   * ! 'visible' in displayWhenVisible refers to the item which is toggled
    * * eg: form, buttons etc
    */
 
-  const displayWhenVisible = { display: isVisible ? '' : 'none' };
-  const hideWhenVisible = { display: isVisible ? 'none' : '' };
+  const displayWhenVisible = { display: isVisible ? '' : 'none' }
+  const hideWhenVisible = { display: isVisible ? 'none' : '' }
 
   const toggleVisible = () => {
-    setIsVisible(!isVisible);
-  };
+    setIsVisible(!isVisible)
+  }
 
   useImperativeHandle(ref, () => {
     return {
       toggleVisible,
-    };
-  });
+    }
+  })
 
   return (
     <div>
@@ -36,13 +36,13 @@ const Togglable = React.forwardRef((props, ref) => {
         <button onClick={toggleVisible}>Cancel</button>
       </div>
     </div>
-  );
-});
+  )
+})
 
-Togglable.displayName = 'Togglable';
+Togglable.displayName = 'Togglable'
 
 Togglable.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
-};
+}
 
-export default Togglable;
+export default Togglable
