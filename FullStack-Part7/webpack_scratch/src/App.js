44f0faf241901733572dc0481eba97c2547ac2fd
dhakalsamundra <seapush02@gmail.com>
2020-11-12 21:01:19 +0200

    add backendurl and useeffect

--- FullStack-Part7/webpack_scratch/src/App.js
+++ FullStack-Part7/webpack_scratch/src/App.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import PromisePolyfill from 'promise-polyfill'
 
-const App = () => (
-    <div className="container">
-    hello webpack</div>
-)
+if (!window.Promise) {
+  window.Promise = PromisePolyfill
+}
 
-export default App
+const useNotes = (url) => {
+    const [notes, setNotes] = useState([])
+    useEffect(() => {
+      axios.get(url).then(response => {
+        setNotes(response.data)
+      })
+    }, [url])
+    return notes
+  }
+
+const App = () => {
+    const [counter, setCounter] = useState(0)
+    const [values, setValues] = useState([])
+    const notes = useNotes(BACKEND_URL)
+  
+    const handleClick = () => {
+      setCounter(counter + 1)
+      setValues(values.concat(counter))
+    }
+  
+    return (
+      <div className="container">
+        hello webpack {counter} clicks
+        <button onClick={handleClick}>
+          press
+        </button>
+        <div>{notes.length} notes on server {BACKEND_URL}</div>
+      </div>
+    )
+  }
+
+  export default App
