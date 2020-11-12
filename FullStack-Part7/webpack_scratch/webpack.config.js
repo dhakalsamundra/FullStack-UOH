e92951f69d1b1c3c88016d141d75005f622dd24b
dhakalsamundra <seapush02@gmail.com>
2020-11-12 20:11:24 +0200

    add transpiler in webpack

--- FullStack-Part7/webpack_scratch/webpack.config.js
+++ FullStack-Part7/webpack_scratch/webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path')
 
 const config = {
-  entry: './src/index.js',
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'main.js'
-  }
+  },
+  //Configuring the loders to transform the JSX code inot regular JavaScript
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+        },
+      },
+    ],
+  },
 }
 module.exports = config
