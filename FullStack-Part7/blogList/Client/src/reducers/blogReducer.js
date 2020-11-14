11f36e56d79b4bd3ba83c5d87c36343a7b5adcf6
dhakalsamundra <seapush02@gmail.com>
2020-11-14 22:11:22 +0200

    blog reducer

---
+++ FullStack-Part7/blogList/Client/src/reducers/blogReducer.js
@@ -0,0 +1,67 @@
+import blogService from '../services/blogs'
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+  case 'ADD_NEW':{
+    const toAdd = action.data
+    return [...state, toAdd]
+  }
+  case 'REMOVE_BLOG':{
+    const removeBlog = action.data
+    const newBlogs = state.filter((blog) => blog.id !== removeBlog.id)
+    return newBlogs
+  }
+  case 'UPDATE_BLOG': {
+    const updateBlog = action.data
+    const updatedBlogs = state.filter((blog) => blog.id !== updateBlog.id)
+    return [...updatedBlogs, updateBlog]
+  }
+  case 'INIT':
+    return action.data
+
+  default:
+    return state
+  }
+}
+
+export const getAll = () => {
+  return async (dispatch) => {
+    const getBlogs = await blogService.getAll()
+    dispatch({
+      type: 'INIT',
+      data: getBlogs,
+    })
+  }
+}
+
+export const addBlog = (blog) => {
+  return async (dispatch) => {
+    const addBlog = await blogService.create(blog)
+    dispatch({
+      type: 'ADD_BLOG',
+      data: addBlog,
+    })
+  }
+}
+
+export const deleteBlog = (blog) => {
+  return async (dispatch) => {
+    const removeBlog = await blogService.remove(blog)
+    dispatch({
+      type: 'REMOVE_BLOG',
+      data: removeBlog,
+    })
+  }
+}
+
+export const likeBlog = (blog) => {
+  return async (dispatch) => {
+    const blogLiked = await blogService.update(blog)
+    dispatch({
+      type: 'UPDATE_BLOG',
+      data: blogLiked
+    })
+  }
+}
+
+export default reducer
