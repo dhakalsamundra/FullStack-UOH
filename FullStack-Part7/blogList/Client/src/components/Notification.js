bcc3f4a3bcfe92f334386bf24caf62ec61ac402e
dhakalsamundra <seapush02@gmail.com>
2020-11-16 20:33:08 +0200

    use redux in notification

--- FullStack-Part7/blogList/Client/src/components/Notification.js
+++ FullStack-Part7/blogList/Client/src/components/Notification.js
@@ -1,9 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-const Notification = ({ notification }) => {
-  if (notification === null) {
-    return null
-  }
+const Notification = () => {
+  const notification = useSelector((state) => state.notification)
+
   return (
     <div className={notification.type}>
       <p>{notification.message}</p>
