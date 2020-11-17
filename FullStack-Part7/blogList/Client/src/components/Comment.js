f50b04fed66e9e6b979120955c1b26787f747331
dhakalsamundra <seapush02@gmail.com>
2020-11-17 20:45:41 +0200

    add component

---
+++ FullStack-Part7/blogList/Client/src/components/Comment.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+
+import { commentBlog } from '../reducers/blogReducer'
+import { useFeild } from '../hooks'
+import { setNotification } from '../reducers/notificationReducer'
+
+const Comment = ({ blog }) => {
+  const dispatch = useDispatch()
+  const [ comment, setComment ] = useFeild()
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    setComment.clear()
+    try {
+      await dispatch(commentBlog({ id: blog.id, content: comment }))
+      dispatch(setNotification('Added', 5))
+    } catch (error) {
+      const msg = 'error'
+      dispatch(setNotification(msg, 5, 'error'))
+    }
+  }
+
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input value={ comment } { ...setComment } placeholder='Comment here' />
+      </form>
+    </div>
+  )
+}
+
+export default Comment
