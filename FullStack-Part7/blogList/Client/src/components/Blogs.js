3f24ccd3b6d26e05dd0dcd4f954e4d231845accc
dhakalsamundra <seapush02@gmail.com>
2020-11-20 09:30:52 +0200

    add styled component

--- FullStack-Part7/blogList/Client/src/components/Blogs.js
+++ FullStack-Part7/blogList/Client/src/components/Blogs.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Button } from '@material-ui/core'
+import Paper from '@material-ui/core/Paper'
+import { makeStyles } from '@material-ui/core/styles'
+import { getAll } from '../reducers/blogReducer'
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(160),
+      height: theme.spacing(10),
+    },
+  },
+}))
 const Blogs = ({ blogs }) => {
-  return(
+  const classes = useStyles()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAll())
+  }, [])
+
+  return (
     <div>
       {blogs.map((blog) => (
-        <div key={blog.id}>
-          {blog.title}
-          <Button color="primary" component={Link} to={`/blogs/${blog.id}`}>Details</Button>
+        <div className={ classes.root}  key={blog.id}>
+          <Paper>
+            <div>
+              {blog.title}
+            </div>
+            <Button color='primary' component={Link} to={`/blogs/${blog.id}`}>
+              Details
+            </Button>
+          </Paper>
         </div>
       ))}
     </div>
