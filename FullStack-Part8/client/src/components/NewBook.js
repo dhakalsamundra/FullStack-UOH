1534372b57708e0cbdce2fd8f7d9d0f2005260b3
dhakalsamundra <seapush02@gmail.com>
2020-11-28 13:40:36 +0200

    exercise complete upto 8.14

--- FullStack-Part8/client/src/components/NewBook.js
+++ FullStack-Part8/client/src/components/NewBook.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useLazyQuery, useMutation } from '@apollo/client'
+import { useMutation } from '@apollo/client'
 
 import { ALL_AUTHORS, CREATE_BOOK, ALL_BOOKS } from '../queries'
 
