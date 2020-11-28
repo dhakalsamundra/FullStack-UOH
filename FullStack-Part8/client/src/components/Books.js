d1f30ea00fe4de12147f36194c0311b66f4ea6e8
dhakalsamundra <seapush02@gmail.com>
2020-11-28 10:08:56 +0200

    add book component

---
+++ FullStack-Part8/client/src/components/Books.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useQuery } from '@apollo/client';
+
+import { ALL_BOOKS } from '../queries'
+import Spinner from './Spinner';
+
+const Persons = ({ show }) => {
+  const result = useQuery(ALL_BOOKS)
+
+  if (!show) {
+    return null;
+  }
+  if (result.loading)  {
+    return <div><Spinner /></div>
+  }
+  const books = result.data.allBooks
+    return (
+      <div>
+      <h2>Books</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th>title</th>
+            <th>published</th>
+            <th>name</th>
+          </tr>
+          {books.map((a) => (
+            <tr key={a.id}>
+              <td>{a.title}</td>
+              <td>{a.published}</td>
+              <td>{a.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    )
+  }
+
+  export default Persons
