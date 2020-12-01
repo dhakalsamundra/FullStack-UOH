ad2fc55f2ee9e26d04c67aeba21e88d45ab96227
dhakalsamundra <seapush02@gmail.com>
2020-12-01 10:10:24 +0200

    add filter function

--- FullStack-Part8/client/src/components/Books.js
+++ FullStack-Part8/client/src/components/Books.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client';
 
 import { ALL_BOOKS } from '../queries'
 import Spinner from './Spinner';
 
-const Persons = ({ show }) => {
+const Book = ({ show }) => {
   const result = useQuery(ALL_BOOKS)
+  const [ filter, setFilter ] = useState('')
 
   if (!show) {
     return null;
@@ -14,6 +15,21 @@ const Persons = ({ show }) => {
     return <div><Spinner /></div>
   }
   const books = result.data.allBooks
+  const filteredBook = () => {
+    if(filter === 'reset') return books
+    else if (filter === '') return books
+    const result = books.filter((book) => book.genres.includes(filter))
+    return result
+  }
+
+  const genres = () => {
+    const samundra = books.map(b=> b.genres)
+    const sam = [ ...samundra].flat()
+      return sam
+  }
+  const handleFilter = (genre) => {
+    setFilter(genre)
+  }
     return (
       <div>
       <h2>Books</h2>
@@ -24,17 +40,22 @@ const Persons = ({ show }) => {
             <th>published</th>
             <th>name</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.id}>
-              <td>{a.title}</td>
-              <td>{a.published}</td>
-              <td>{a.author.name}</td>
+          {filteredBook().map((b) => (
+            <tr key={b.id}>
+              <td>{b.title}</td>
+              <td>{b.published}</td>
+              <td>{b.author.name}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      {genres().map(genre => (
+        <button onClick={() => handleFilter(genre)}>{genre}</button>
+      )
+      )}
+      <button onClick={() => handleFilter('reset')}>books</button>
     </div>
     )
   }
 
-  export default Persons
+  export default Book
