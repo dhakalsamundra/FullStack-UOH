5629f4c1eb71f4147ec5a3033e7bf5193a0155f7
dhakalsamundra <seapush02@gmail.com>
2020-12-01 10:09:16 +0200

    add login

---
+++ FullStack-Part8/client/src/components/LoginForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import {  useMutation } from '@apollo/client';
+
+import { LOGIN } from '../queries'
+
+const LoginForm = ({ show, setToken, setPage }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [ login ] = useMutation(LOGIN)
+
+  if (!show) {
+    return null
+  }
+
+  const submit = async event => {
+    event.preventDefault()
+
+    const result = await login({
+      variables: { username, password }
+    })
+
+    if (result) {
+      const token = result.data.login.value
+      setToken(token)
+      localStorage.setItem('user', token)
+      setPage('authors')
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          username{' '}
+          <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password{' '}
+          <input
+            type='password'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
