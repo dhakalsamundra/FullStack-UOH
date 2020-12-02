5df0602f5e5ba89e33e4724ad4a98e4e33a37f73
dhakalsamundra <seapush02@gmail.com>
2020-12-02 11:00:36 +0200

    set birth date to not mandotary

--- FullStack-Part8/server/typeDefs.js
+++ FullStack-Part8/server/typeDefs.js
@@ -41,7 +41,7 @@ module.exports = gql`
       published: Int!
       genres: [String!]!
     ): Book
-    editAuthor(name: String!, setBornTo: Int!): Author
+    editAuthor(name: String!, setBornTo: Int): Author
     createUser(username: String!, favoriteGenre: String!): User
     login(username: String!, password: String!): Token
   }
