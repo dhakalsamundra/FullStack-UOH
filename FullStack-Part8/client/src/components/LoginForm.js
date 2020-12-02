de3fc82428646047c1b96e89f72011bee7326b14
dhakalsamundra <seapush02@gmail.com>
2020-12-02 15:37:17 +0200

    add notification for login

--- FullStack-Part8/client/src/components/LoginForm.js
+++ FullStack-Part8/client/src/components/LoginForm.js
@@ -1,56 +1,65 @@
-import React, { useState } from 'react'
-import {  useMutation } from '@apollo/client';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
 
-import { LOGIN } from '../queries'
+import { LOGIN } from "../queries";
 
-const LoginForm = ({ show, setToken, setPage }) => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+const LoginForm = ({ show, setToken, setPage, notify }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [ login ] = useMutation(LOGIN)
+  const [login] = useMutation(LOGIN, {
+    onError: (error) => {
+      notify(error.graphQLErrors[0].message);
+    },
+    onCompleted: (data) => {
+      const token = data.login.value;
+      setToken(token);
+      localStorage.setItem("user", token);
+      setPage("authors");
+    },
+  });
 
   if (!show) {
-    return null
+    return null;
   }
 
-
-  const submit = async event => {
-    event.preventDefault()
+  const submit = async (event) => {
+    event.preventDefault();
 
     const result = await login({
-      variables: { username, password }
-    })
+      variables: { username, password },
+    });
 
     if (result) {
-      const token = result.data.login.value
-      setToken(token)
-      localStorage.setItem('user', token)
-      setPage('authors')
+      const token = result.data.login.value;
+      setToken(token);
+      localStorage.setItem("user", token);
+      setPage("authors");
     }
-  }
+  };
 
   return (
     <div>
       <form onSubmit={submit}>
         <div>
-          username{' '}
+          username{" "}
           <input
             value={username}
             onChange={({ target }) => setUsername(target.value)}
           />
         </div>
         <div>
-          password{' '}
+          password{" "}
           <input
-            type='password'
+            type="password"
             value={password}
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
-        <button type='submit'>login</button>
+        <button type="submit">login</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
