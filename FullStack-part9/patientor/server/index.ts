a3f9ae390d756b6bc9dccbb5060ae6133fa5aa79
dhakalsamundra <seapush02@gmail.com>
2020-12-05 13:38:06 +0200

    add patient endpoint

--- FullStack-part9/patientor/server/index.ts
+++ FullStack-part9/patientor/server/index.ts
@@ -1,12 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 
+import patients from './data/patients.json'
+import diagnoses from './data/diagnoses.json'
+
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 app.get('/api/ping', (_req, res) => {
     res.status(200).send('pong')
+})
+
+app.get('/api/patients', (_req, res) => {
+    res.status(200).send(patients)
 })
 
 const PORT = 3001
