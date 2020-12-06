790a77afebd2257da1ac470d96a6cafb273a1eaa
dhakalsamundra <seapush02@gmail.com>
2020-12-06 17:07:08 +0200

    error fix on route

--- FullStack-part9/patientor/server/index.ts
+++ FullStack-part9/patientor/server/index.ts
@@ -1,8 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 
-import patients from './data/patients.json'
-import diagnoses from './data/diagnoses.json'
+import diagnosesRouter from './routes/diagonses'
+import patientsRouter from './routes/patients'
 
 const app = express()
 app.use(express.json())
@@ -12,9 +12,8 @@ app.get('/api/ping', (_req, res) => {
     res.status(200).send('pong')
 })
 
-app.get('/api/patients', (_req, res) => {
-    res.status(200).send(patients)
-})
+app.use('/api/diagnoses', diagnosesRouter)
+app.use('/api/patients', patientsRouter)
 
 const PORT = 3001
 
