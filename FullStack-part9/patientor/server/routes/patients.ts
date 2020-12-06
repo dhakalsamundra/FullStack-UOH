2de35d7ef3ddfd6bc6434719c81f910d8768d69a
dhakalsamundra <seapush02@gmail.com>
2020-12-06 17:08:16 +0200

    add patient route

--- FullStack-part9/patientor/server/routes/patients.ts
+++ FullStack-part9/patientor/server/routes/patients.ts
@@ -1,7 +1,23 @@
 import express from 'express'
 
+import service from '../service'
+import toNewPatientsEntry from '../utils'
+
 const router = express.Router()
 
 router.get('/', (_req, res) => {
-    
+    const getData = service.getPatientsData()
+    res.status(200).send(getData)
 })
+
+router.post('/', (req, res) => {
+    try{
+        const newPatient = toNewPatientsEntry(req.body)
+        const addNewPatient = service.addPatientsData(newPatient)
+        res.status(201).send(addNewPatient)
+    } catch (e) {
+        res.status(500).send({ error: e.message })
+    }
+})
+
+export default router
