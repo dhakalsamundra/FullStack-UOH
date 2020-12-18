be5203f3c67e65eb653ba64118966f8373be3b71
dhakalsamundra <seapush02@gmail.com>
2020-12-18 12:58:05 +0200

    error solve

--- FullStack-Part5/src/components/Blog.test.js
+++ FullStack-Part5/src/components/Blog.test.js
@@ -1,54 +1,53 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render, fireEvent } from '@testing-library/react';
-import Blog from './Blog';
-import EachBlog from './EachBlog'
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/react'
+import Blog from './Blog'
 
 describe('<Blog />', () => {
 
-  let component;
-  let addLikes;
+  let component
+  let addLikes
 
   beforeEach(() => {
-    addLikes = jest.fn();
+    addLikes = jest.fn()
     const blog = {
       author: 'Samundra',
       title: 'Software Testing',
       url: 'test.com'
-    };
+    }
 
-    component = render(<Blog blog={blog} handleChange={addLikes} />);
-  });
+    component = render(<Blog blog={blog} addLikes={addLikes} />)
+  })
 
   test('render blogs', () => {
-    expect(component.container).toHaveTextContent('Samundra');
-  });
+    expect(component.container).toHaveTextContent('Samundra')
+  })
 
   test('only title and author feild will be visible', () => {
-    const data = component.container.querySelector('.blog');
-    expect(data).toBeDefined();
+    const data = component.container.querySelector('.blog')
+    expect(data).toBeDefined()
 
-    const detail = component.container.querySelector('.showDetails');
-    expect(detail).toBeNull();
-  });
+    const detail = component.container.querySelector('.showDetails')
+    expect(detail).toBeNull()
+  })
 
   test('details will be shown when show button is clicked', () => {
-    const showButton = component.getByText('Show');
-    fireEvent.click(showButton);
+    const showButton = component.getByText('Show')
+    fireEvent.click(showButton)
 
-    const detail = component.container.querySelector('.showDetails');
-    expect(detail).toBeDefined();
-  });
+    const detail = component.container.querySelector('.showDetails')
+    expect(detail).toBeDefined()
+  })
 
   test('like button calls addLikes', () => {
-    const showButton = component.getByText('Show');
-    fireEvent.click(showButton);
+    const showButton = component.getByText('Show')
+    fireEvent.click(showButton)
 
-    const likeButton = component.getByText('Like');
+    const likeButton = component.getByText('Like')
     //clicking two times like button
-    fireEvent.click(likeButton);
-    fireEvent.click(likeButton);
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
 
-    expect(addLikes.mock.calls).toHaveLength(2);
-  });
-});
+    expect(addLikes.mock.calls).toHaveLength(2)
+  })
+})
